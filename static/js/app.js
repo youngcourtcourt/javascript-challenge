f94421002e99ecb250252a128bd6eb5959b00f80
youngcourtcourt <courtneynicolemacdonald@gmail.com>
2021-04-05 13:32:48 -0700

    Able to append data to webpage, but is appending horizontally instead of vertically

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,20 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+var table=d3.select("#ufo-table")
+
+var body=d3.select("tbody")
+
+var row=body.append("tr")
+
+// row.append("td").text()
+
+// data.forEach(entry=>{
+//     row.append("td").text(entry.date)
+//     row.append("td").text(entry.city)
+//     row.append("td").text(entry.state)
+//     row.append("td").text(entry.country)
+//     row.append("td").text(entry.durationMinutes)
+//     row.append("td").text(entry.comments)
+// })
