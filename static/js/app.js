ae8ed04536a456c8d65cf24be9fc752b307c8011
youngcourtcourt <courtneynicolemacdonald@gmail.com>
2021-04-05 15:26:32 -0700

    Got table appended to html

--- static/js/app.js
+++ static/js/app.js
@@ -7,15 +7,20 @@ var table=d3.select("#ufo-table")
 
 var body=d3.select("tbody")
 
-var row=body.append("tr")
+
+
 
 // row.append("td").text()
 
-// data.forEach(entry=>{
-//     row.append("td").text(entry.date)
-//     row.append("td").text(entry.city)
-//     row.append("td").text(entry.state)
-//     row.append("td").text(entry.country)
-//     row.append("td").text(entry.durationMinutes)
-//     row.append("td").text(entry.comments)
-// })
+data.forEach(entry=>{
+
+    var row=body.append("tr")
+
+    
+    Object.entries(entry).forEach(([key, value])=>{
+    
+        var cell=row.append("td")
+        cell.text(value)
+
+    })
+})
