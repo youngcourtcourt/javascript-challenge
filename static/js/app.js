4b04a008a8ef00732ad5eeeada1d7a4d8f4b7224
youngcourtcourt <courtneynicolemacdonald@gmail.com>
2021-04-05 17:37:54 -0700

    Able to filter data, still trying to display to table

--- static/js/app.js
+++ static/js/app.js
@@ -24,3 +24,38 @@ data.forEach(entry=>{
 
     })
 })
+
+// Select button and form and assign selection to variables
+
+var button=d3.select("#filter-btn")
+
+var form=d3.select("form")
+
+// Set change event on selected items
+
+button.on("click", runEnter)
+
+form.on("submit", runEnter)
+
+// Select input element that will contain our user input
+
+var inputElement=d3.select("#datetime")
+
+function runEnter(){
+
+    d3.event.preventDefault()
+
+    // Retireve value from input element
+
+    var inputValue=inputElement.property("value")
+
+// Filter data based on user input
+    
+
+    var filteredData=data.filter(data=>data.datetime===inputValue)
+
+    console.log(filteredData)
+
+}
+
+
